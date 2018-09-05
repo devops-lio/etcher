4ec38d3432184e115a6cd9b93b2b98dd0ddcd193
amdomanska <aga@resin.io>
2018-09-05 14:42:33 +0100

    Create basic ProgressButton react component using react2angular
    Signed-off-by: amdomanska <aga@resin.io>

---
+++ lib/gui/app/components/progress-button/progress-button.jsx
@@ -0,0 +1,78 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const propTypes = require('prop-types')
+
+import { Button, ProgressBar, Provider } from 'rendition';
+
+export default class ProgressButton extends React.Component {
+
+  constructor () {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      openBar: false
+    }
+  }
+
+  handleClick () {
+    this.setState({ openBar: true })
+  }
+
+  render () {
+    if (this.state.openBar) {
+      return (
+        <Bar/>
+      )
+    }
+    else {
+      return (
+        <Provider>
+          <Button primary onClick={this.handleClick}>
+            Click!
+          </Button>
+        </Provider>
+      )
+    }
+  }
+}
+
+class Bar extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      value: 0
+    }
+  }
+
+  componentDidMount () {
+    this.progressInterval = setInterval(() => {
+      let value = this.state.value + 1
+      if (value > 100) {
+        value = 0
+      }
+      this.setState({ value })
+    }, 250)
+  }
+
+  componentWillUnmount () {
+    window.clearInterval(this.progressInterval)
+  }
+
+  render () {
+    return (
+      <Provider>
+        <ProgressBar primary emphasized value={this.state.value}>
+          {this.state.value}%
+        </ProgressBar>
+      </Provider>
+    )
+  }
+}
+
+ProgressButton.propTypes = {
+  path: propTypes.string,
+}
+
+module.exports = ProgressButton
