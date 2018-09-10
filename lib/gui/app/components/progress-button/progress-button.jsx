6fc2750c31887d7ca4c806e5aed456e266670da5
amdomanska <aga@resin.io>
2018-09-10 14:25:11 +0100

    Add styled components wrap for scss
    Signed-off-by: amdomanska <aga@resin.io>

--- lib/gui/app/components/progress-button/progress-button.jsx
+++ lib/gui/app/components/progress-button/progress-button.jsx
@@ -1,10 +1,20 @@
-'use strict'
-
 const React = require('react')
 const ReactDOM = require('react-dom')
 const propTypes = require('prop-types')
 
-import { Button, ProgressBar, Provider } from 'rendition';
+import styled, { ThemeProvider } from 'styled-components'
+import { Button, ProgressBar, Provider } from 'rendition'
+
+import createTheme from 'styled-components-theme'
+// import theme from './../../scss/modules/_theme'
+
+const StyledButton = styled(Button).attrs({
+  className: "flash-button"
+})``
+
+const StyledProgressBar = styled(ProgressBar).attrs({
+  className: "flash-progress-bar"
+})``
 
 export default class ProgressButton extends React.Component {
 
@@ -12,13 +22,14 @@ export default class ProgressButton extends React.Component {
     super(props)
 
     this.state = {
-      openBar: false,
       disabled: false,
+      striped: false
     }
   }
 
-  static getDerivedStateFromProps = (props, state) => {
+  static getDerivedStateFromProps (props, state) {
     state.disabled = props.shouldFlashStepBeDisabled || props.getLastFlashErrorCode
+    state.striped = props.striped == 'check'
   }
 
   handleClick = () => {
@@ -26,63 +37,48 @@ export default class ProgressButton extends React.Component {
     this.props.flash(this.props.image(),this.props.device())
   }
 
-  render = () => {
-    if (this.state.openBar) {
+  render () {
+    console.log(this.props.active)
+    if (this.props.active) {
       return (
         <Provider>
-          <ProgressBar primary emphasized value={this.props.percentage}>
-            {this.props.percentage}%
-          </ProgressBar>
+          <StyledProgressBar
+            primary
+            emphasized
+            value={this.props.percentage}
+            striped={this.state.striped}
+          >
+            {this.props.label}
+          </StyledProgressBar>
         </Provider>
       )
     }
     else {
       return (
         <Provider>
-          <Button primary onClick={this.handleClick} disabled={this.state.disabled}>
+          <StyledButton
+            primary
+            onClick={this.handleClick}
+            disabled={this.state.disabled}
+          >
             {this.props.label}
-          </Button>
+          </StyledButton>
         </Provider>
       )
     }
   }
 }
 
-/* class Bar extends React.Component {
-
-  constructor(percentage) {
-    super(percentage)
-    console.log(this.props.percentage)
-    this.state = {
-      value: this.props.percentage().percentage
-    }
-  }
-
-  componentDidMount = () => {
-    console.log("componentdidmount: ", this.props.percentage().percentage)
-    this.setState({value: this.props.percentage()})
-  }
-
-  render = () => {
-    return (
-      <Provider>
-        <ProgressBar primary emphasized value={this.state.value}>
-          {this.state.value}%
-        </ProgressBar>
-      </Provider>
-    )
-  }
-} */
-
 ProgressButton.propTypes = {
-  percentage: propTypes.func,
+  striped: propTypes.func,
+  active: propTypes.func,
+  percentage: propTypes.number,
   label: propTypes.string,
   shouldFlashStepBeDisabled: propTypes.func,
   getLastFlashErrorCode: propTypes.func,
   flash: propTypes.func,
   image: propTypes.func,
   device: propTypes.func,
-  flash: propTypes.func
 }
 
 module.exports = ProgressButton
